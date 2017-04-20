f0b260abd38dc110bcd0cd9315aa717b69c3e4fc
zhwz <zhwz@zhwzdeMacBook-Pro.local>
2017-04-20 16:25:51 +0800

    新建个文件夹试一试

---
+++ Helloworld/TestClass.h
@@ -0,0 +1,13 @@
+//
+//  TestClass.h
+//  Helloworld
+//
+//  Created by zhwz on 2017/4/20.
+//  Copyright © 2017年 zhwz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestClass : NSObject
+
+@end
---
+++ Helloworld/TestClass.m
@@ -0,0 +1,13 @@
+//
+//  TestClass.m
+//  Helloworld
+//
+//  Created by zhwz on 2017/4/20.
+//  Copyright © 2017年 zhwz. All rights reserved.
+//
+
+#import "TestClass.h"
+
+@implementation TestClass
+
+@end
